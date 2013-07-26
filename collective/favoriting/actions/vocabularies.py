5363c750d69c2d4978bf3800c25528a821ed1994
Gagaro <gagaro42@gmail.com>
2013-07-26 11:35:54 +0200

    fix: python validation

--- collective/favoriting/actions/vocabularies.py
+++ collective/favoriting/actions/vocabularies.py
@@ -3,6 +3,6 @@ from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from collective.favoriting.i18n import _
 
 favoritingChoice = SimpleVocabulary([
-        SimpleTerm('favorite', 'favorite', _(u'Add to favorite')),
-        SimpleTerm('unfavorite', 'unfavorite', _(u'Remove from favorite')),
-        ])
+    SimpleTerm('favorite', 'favorite', _(u'Add to favorite')),
+    SimpleTerm('unfavorite', 'unfavorite', _(u'Remove from favorite')),
+    ])
